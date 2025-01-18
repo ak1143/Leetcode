ed8d99cd1895ce4471a9ba5e944915fbbaee2dea
Abhishek kshirsagar <126000789+ak1143@users.noreply.github.com>
2025-01-19 03:56:42 +0530

    Time: 2 ms (100%), Space: 62.1 MB (69.36%) - LeetHub

--- 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.java
+++ 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.java
@@ -1,27 +1,12 @@
 class Solution {
     public boolean doesValidArrayExist(int[] derived) {
-        int n = derived.length;
-        int[] original = new int[n];
-
-        boolean result = check(original,derived,0);
-        if(result == true) return true;
-
-        result = check(original,derived,1);
-        if(result == true) return true;
-
-        return false;
-
-    }
-
-    public boolean check(int []or,int []der, int a){
-        or[0] = a;
-
-        for(int i=1;i<or.length;i++)
+        int result = 0;
+        for(int num : derived)
         {
-            or[i] = or[i-1] ^ der[i-1]; 
+            result = result ^ num; 
         }
 
-        if((or[or.length-1] ^ or[0]) == der[or.length-1]) return true;
+        if(result == 0) return true;
 
         return false;
     }
