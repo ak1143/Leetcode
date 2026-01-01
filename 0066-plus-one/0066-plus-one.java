cccd216cab243d08d7e6e7ccc7aa8aacc6b87d33
Abhishek kshirsagar <126000789+ak1143@users.noreply.github.com>
2026-01-01 21:30:46 +0530

    Time: 0 ms (100%), Space: 40.9 MB (99.99%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,31 +1,16 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        int n = digits.length-1;
-        if(digits[n]!=9){
-            digits[n]= digits[n]+1;
-            return digits;
-        }
-
-        if(allNines(digits)){
-            int []arr = new int[n+2];
-            arr[0] = 1;
-            return arr;
-        }
-
-        int i=n;
-
-        while(digits[i]==9 && i>=0){
+        for(int i=digits.length-1;i>=0;i--)
+        {
+            if(digits[i]<9)
+            {
+                digits[i]++;
+                return digits;
+            }
             digits[i]=0;
-            i--;
         }
-        digits[i] = digits[i]+1;
+        digits =new int[digits.length+1];
+        digits[0]=1;
         return digits;
-
-    }
-    public boolean allNines(int[]arr){
-        for(int i:arr){
-            if(i!=9) return false;
-        }
-        return true;
     }
 }
