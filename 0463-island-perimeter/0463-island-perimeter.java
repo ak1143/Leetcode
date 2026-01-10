18dd0d43e06dab18e4cd9bbd1f8fd5886a38c5fc
Abhishek kshirsagar <126000789+ak1143@users.noreply.github.com>
2026-01-10 14:42:24 +0530

    Time: 6 ms (32.15%), Space: 47.5 MB (19.1%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.java
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -1,38 +1,30 @@
 class Solution {
-    
-    int count=0;
+    int parameter = 0;
     public int islandPerimeter(int[][] grid) {
-        
-        for(int i=0;i<grid.length;i++)
-        {
-            for(int j=0;j<grid[0].length;j++)
-            {
-                if(grid[i][j]==1)
-                {
-                    dfs(grid,i,j);
+        for(int i =0 ;i<grid.length;i++){
+            for(int j=0;j<grid[i].length;j++){
+                if(grid[i][j]==1){
+                    helper(grid,i,j);
                     break;
                 }
             }
         }
-        
-        return count;
+        return parameter;
     }
-    
-    private void dfs(int[][] grid,int i,int j)
-    {
-        if(i<0 || i>=grid.length || j<0 || j>=grid[i].length || grid[i][j]==0)
-        {
-            count++;
+    public void helper(int[][] grid,int i, int j){
+
+        if(i<0 || i>=grid.length || j<0 || j>=grid[i].length || grid[i][j]==0){
+            parameter+=1;
             return ;
         }
-        
-        if(grid[i][j]==-1)  return;
-        
+        if(grid[i][j]==-1) return ;
         grid[i][j]=-1;
-        
-        dfs(grid,i+1,j);
-        dfs(grid,i-1,j);
-        dfs(grid,i,j+1);
-        dfs(grid,i,j-1);
+
+        helper(grid,i+1,j);
+        helper(grid,i-1,j);
+        helper(grid,i,j+1);
+        helper(grid,i,j-1);
+
+
     }
 }
