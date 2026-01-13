faa9ff602795051b6aed3bbd265ed68136d34aa5
Abhishek kshirsagar <126000789+ak1143@users.noreply.github.com>
2026-01-13 23:01:59 +0530

    Time: 1 ms (100%), Space: 43 MB (46.13%) - LeetHub

--- 2600-k-items-with-the-maximum-sum/2600-k-items-with-the-maximum-sum.java
+++ 2600-k-items-with-the-maximum-sum/2600-k-items-with-the-maximum-sum.java
@@ -1,7 +1,5 @@
 class Solution {
     public int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
-        if(numOnes >= k ) return k;
-        else if(numOnes + numZeros >= k) return numOnes;
-        return numOnes- (k - (numOnes + numZeros));
+        return (numOnes >= k) ? k : (numOnes + numZeros >= k ? numOnes : numOnes- (k - (numOnes + numZeros)));
     }
 }
