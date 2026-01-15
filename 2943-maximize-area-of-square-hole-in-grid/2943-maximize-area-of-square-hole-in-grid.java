d69fd9c89ffe0da8161dc3c37b3d624eb6d6c0db
Abhishek kshirsagar <126000789+ak1143@users.noreply.github.com>
2026-01-15 17:04:33 +0530

    Time: 4 ms (100%), Space: 45.6 MB (26.47%) - LeetHub

--- 2943-maximize-area-of-square-hole-in-grid/2943-maximize-area-of-square-hole-in-grid.java
+++ 2943-maximize-area-of-square-hole-in-grid/2943-maximize-area-of-square-hole-in-grid.java
@@ -14,8 +14,8 @@ class Solution {
     int findMaxConsiqutive(int[]arr){
         int countMaxlen =1;
         int count =1;
-        for(int i=0;i<arr.length-1;i++){
-            if(arr[i]+1==arr[i+1]){
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]==arr[i-1]+1){
                 count++;
                 countMaxlen = Math.max(count,countMaxlen);
             }
