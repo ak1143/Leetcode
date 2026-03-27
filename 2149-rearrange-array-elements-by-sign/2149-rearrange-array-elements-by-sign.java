546187b4437e5f35d43d9554c57a35ddeac9d6e3
Abhishek kshirsagar <126000789+ak1143@users.noreply.github.com>
2026-03-28 01:53:54 +0530

    Time: 4 ms (42.7%), Space: 172.4 MB (91.69%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,18 +1,54 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        int p = 0, n = 1;
-        int[] res = new int[nums.length];
+
+        // // even -> positive
+        // // old -> neg
+
+        // int i=0,j=0;
+
+        // while(i<n){
+        //     if(nums[i]>0) break;
+        //     i++;
+        // }
+
+        // while(j<n){
+        //     if(nums[j]<0) break;
+        //     j++;
+        // }
+
+        // while(j<nums.length){
         
-        for(int i= 0;i<res.length;i++){
-            if(nums[i]>0){
-                res[p] = nums[i];
-                p += 2;
-            }
-            else{
-                res[n] = nums[i];
-                n += 2;
+        //     if(i==0) continue;
+
+        //     swap(i,j);
+
+        // }
+
+        int n = nums.length;
+        int k=0,l=0;
+        int [] arr1 = new int[n/2];
+
+        int [] arr2 = new int[n/2];
+
+        for(int i=0;i<n;i++){
+            if(nums[i]<0){
+                arr1[k] = nums[i];
+                k++; 
+            }else{
+                arr2[l]= nums[i];
+                l++;
             }
         }
-        return res;
+        k=0;
+        l=0;
+
+        for(int i=0;i<n;i+=2){
+            nums[i] = arr2[l++];
+            nums[i+1] = arr1[k++];
+        }
+
+        return nums;
+
+
     }
 }
